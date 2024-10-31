95663872a7c8da950bb9a9c69f571680c2498a13
Mathis-Bsnrd <mathisbesnard3@gmail.com>
2024-10-31 14:42:15 +0100

    Initialize project

---
+++ src/main/java/com/vbc/website/VbcWebsiteBackApplication.java
@@ -0,0 +1,13 @@
+package com.vbc.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbcWebsiteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbcWebsiteBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vbc/website/VbcWebsiteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbc.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbcWebsiteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
