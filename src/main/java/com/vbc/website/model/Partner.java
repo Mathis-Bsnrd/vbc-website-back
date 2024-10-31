188275480e65036b8412a4f429544a4f69a13d7b
Mathis-Bsnrd <mathisbesnard3@gmail.com>
2024-10-31 17:47:57 +0100

    Create model for database table creation

---
+++ src/main/java/com/vbc/website/model/Partner.java
@@ -0,0 +1,16 @@
+package com.vbc.website.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Partner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String image;
+    private String partnerUrl;
+}
